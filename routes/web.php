865cc018478062646c96c7001b19d73e2c7be389
Raju Mondal <raju.mondal@winexsoft.com>
2021-07-10 20:05:15 +0600

    send mail notification with queue

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Models\User;
+use App\Notifications\TaskCompleted;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
+//    $delay = Carbon::now()->addSecond(10);
+//    User::find(1)->notify((new TaskCompleted)->delay($delay));
+
+//    $user = User::find(1);
+//    Notification::send($user, new TaskCompleted);
+
+    $user = User::find(1);
+    $delay = Carbon::now()->addSecond(3);
+    Notification::route('mail', 'rajumondalr82@gmail.com')
+        ->notify((new TaskCompleted($user))->delay($delay));
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
